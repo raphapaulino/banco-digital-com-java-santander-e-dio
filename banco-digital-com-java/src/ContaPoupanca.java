9cc0dcff9366559ffe4b2d9b111de004e4be86aa
Raphael Paulino <raphael.paulino.web@gmail.com>
2022-08-07 18:06:31 -0300

    feat: create the IConta interface and apply heritage

--- banco-digital-com-java/src/ContaPoupanca.java
+++ banco-digital-com-java/src/ContaPoupanca.java
@@ -1,18 +1,3 @@
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta {
 
-    private int agencia;
-    private int numero;
-    private double saldo;
-
-    public void sacar() {
-
-    }
-
-    public void depositar() {
-
-    }
-
-    public void transferir() {
-
-    }
 }
